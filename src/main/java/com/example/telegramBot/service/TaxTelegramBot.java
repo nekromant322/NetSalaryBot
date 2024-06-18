3f001671d75369cf2de3c9ea11a59fc2649db66f
CoolCat100 <xxdenxxx@mail.ru>
2024-06-19 00:22:38 +0300

    Fixed incorrect posting to topic

--- src/main/java/com/example/telegramBot/service/TaxTelegramBot.java
+++ src/main/java/com/example/telegramBot/service/TaxTelegramBot.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.slf4j.Logger;
